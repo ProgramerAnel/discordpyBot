f3b39b3941dfc2ace8151a55eec27cd9422900bb
Anel Hodžić <iml4203@Imel.Local>
2022-09-02 13:03:09 +0200

    Python done !

--- db_SQL.py
+++ db_SQL.py
@@ -25,19 +25,19 @@ class MyModal(Modal):
         self.add_item(
             InputText(
                 label="Sold site",
-                placeholder="Poshmark" 
+                value="Poshmark" 
             )
         )
         self.add_item(
             InputText(
                 label="Sold price",
-                placeholder="xxxx$" 
+                value="xxxx$" 
             )
         )
         self.add_item(
             InputText(
                 label="Sold note",
-                placeholder="Note",style=discord.InputTextStyle.long 
+                value="Description",style=discord.InputTextStyle.long 
             )
         )
     async def callback(self,interaction:discord.Interaction):
@@ -45,45 +45,11 @@ class MyModal(Modal):
         _site = self.children[1].value
         _price = self.children[2].value
         _note = self.children[3].value
- 
-        sql = '''SELECT
-            CASE WHEN Active = 1 THEN 1 Else 0 End AS ActiveEbay,
-            CASE WHEN Active = 1 then 'https://www.ebay.com/itm/' + cast(ebay_id as nvarchar ) else null end as EbayLink,
-            CASE WHEN ActiveEtsy  = 1 THEN 1 Else 0 End AS ActiveEtsy ,
-            CASE WHEN ActiveEtsy  = 1 then 'https://www.etsy.com/listing/' + cast(ETSY_ID as nvarchar ) else null end as EtsyLink,
-            CASE WHEN ActivePoshmark  = 1 THEN 1 Else 0 End AS ActivePoshmark ,
-            CASE WHEN ActivePoshmark  = 1 then POSHMARK_LINK else null end as PoshmarkLink,
-            CASE WHEN activemercari  = 1 THEN 1 Else 0 End AS ActiveMercari ,
-            CASE WHEN activemercari  = 1 then MERCARI_LINK else null end as MercariLink,
-            ID, Title,(select top 1 PICTURE_PATH from ACTIVE_ITEMS_PICTURES where ACTIVE_ITEMS_PICTURES.ACTIVE_ITEMS_ID = ACTIVE_ITEMS.ID) IMG
-        FROM ACTIVE_ITEMS where sku = ?'''
-        
-        myData = myDB.execute_and_fetch(sql,_sku)
-        for ActiveEbay,EbayLink,ActiveEtsy,EtsyLink,ActivePoshmark,PoshmarkLink,ActiveMercari,MercariLink,ID,Title,IMG in myData:
- 
-            embMsg=discord.Embed(title=f"#{_sku} Sold for {_price}$ on {_site}", url=EbayLink, description=Title, color=0xe71212)
-  
-            embMsg.add_field(name="Poshmark", value=PoshmarkLink, inline=False)
-            embMsg.add_field(name="Etsy", value=EtsyLink, inline=False)
-            embMsg.add_field(name="Ebay", value=EbayLink, inline=False)
-            embMsg.add_field(name="Mercari", value=MercariLink, inline=False) 
-            embMsg.timestamp = datetime.datetime.utcnow()
-            embMsg.set_footer(text='Bot by programerAnel@gmail.com',icon_url= 'https://i.imgur.com/wSTFkRM.png')
-            embMsg.set_thumbnail(url=IMG)
-            # if ActiveEbay==1:
-            #     myDB.execute("INSERT INTO QUEUE(ACTIVE_ITEMS_ID,SITE,TYPER)VALUES('?','Ebay','End')",ID)
-            # if ActiveEtsy==1:
-            #     myDB.execute("INSERT INTO QUEUE(ACTIVE_ITEMS_ID,SITE,TYPER)VALUES('?','Etsy','End')",ID)
-            # if ActivePoshmark:
-            #     myDB.execute("INSERT INTO QUEUE(ACTIVE_ITEMS_ID,SITE,TYPER)VALUES('?','Poshmark','End')",ID)
-            # if ActiveMercari:
-            #     myDB.execute("INSERT INTO QUEUE(ACTIVE_ITEMS_ID,SITE,TYPER)VALUES('?','Mercari','End')",ID)
 
+        myDB.execute("UPDATE sellduct.DBO.ACTIVE_ITEMS SET sold_price = ?, Active = 0, sold_site = ?, SOLD_DATE = getdate(),SOLD_NOTE = ? WHERE SKU = ? ", _price,
+                    _site, _note, _sku)
 
-        # myDB.execute("UPDATE sellduct.DBO.ACTIVE_ITEMS SET sold_price = ?, Active = 0, sold_site = ?, SOLD_DATE = getdate(),SOLD_NOTE = ? WHERE SKU = ? ", _price,
-        #             _site, _note, _sku)
-
-        await interaction.response.send_message(embed=embMsg)
+        await interaction.response.send_message(f"{val1} and {val2}")
 
 @bot.event
 async def on_ready():
@@ -106,7 +72,7 @@ async def on_message(message):
 
 async def getSqlData(sku, message):
     mySQLData = myDB.execute_and_fetch('select Mercari_price,POSHMARK_PRICE,Title,Price,isnull(Active,0) Active,ActiveEtsy,ActivePoshmark,activemercari,POSHMARK_LINK,MERCARI_LINK,SPECIFICS_JSON,ebay_id,(select top 1 PICTURE_PATH from ACTIVE_ITEMS_PICTURES where ACTIVE_ITEMS_PICTURES.ACTIVE_ITEMS_ID = ACTIVE_ITEMS.ID) IMG from Active_Items where SKU = ?', sku) 
-    for Mercari_price,POSHMARK_PRICE,Title,Price,Active,ActiveEtsy,ActivePoshmark,activemercari,POSHMARK_LINK,MERCARI_LINK,SPECIFICS_JSON,ebay_id in mySQLData:
+    for Mercari_price,POSHMARK_PRICE,Title,Price,Active,ActiveEtsy,ActivePoshmark,activemercari,POSHMARK_LINK,MERCARI_LINK,SPECIFICS_JSON,ebay_id,IMG in mySQLData:
         myTitle = Title
         myEbayID = ebay_id
 
@@ -130,7 +96,7 @@ async def getSqlData(sku, message):
         )
     embMsg.set_author(name=_state,url=_img,icon_url=_img)
     # embedVar.set_author(_state, _img, 'https://discord.js.org')
-    # embMsg.set_thumbnail(url=IMG)
+    embMsg.set_thumbnail(url=IMG)
 
     embMsg.add_field(name="Price [Ebay]", value=Price, inline=True)
     embMsg.add_field(name="Price [Poshmark]", value=POSHMARK_PRICE, inline=True)
